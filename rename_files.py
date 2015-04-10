3ab34e663cea78a39be3e44f8b6e3b82cddb8567
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-10 15:34:44 +0200

    Get file names from a folder

--- rename_files.py
+++ rename_files.py
@@ -2,3 +2,8 @@
 import os
 
 def rename_files():
+    #(1) get file names from a folder
+    file_list = os.listdir("/Users/eliasvensson/Udacity/full-stack/PFWP/rename_files/prank")
+    os.chdir("/Users/eliasvensson/Udacity/full-stack/PFWP/rename_files/prank")
+    saved_path = os.getcwd()
+    
