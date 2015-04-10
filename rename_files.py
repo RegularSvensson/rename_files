664b4bfa98c15505850794f5fdd3cd5adedd06d2
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-10 15:34:54 +0200

    For each file, rename filename

--- rename_files.py
+++ rename_files.py
@@ -7,3 +7,10 @@ def rename_files():
     os.chdir("/Users/eliasvensson/Udacity/full-stack/PFWP/rename_files/prank")
     saved_path = os.getcwd()
     
+    #(2) for each file, rename filename
+    for file_name in file_list:
+    	print("Old Name - " + file_name)
+    	print("New Name - " + file_name.translate(None, "0123456789"))
+    	os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
+
